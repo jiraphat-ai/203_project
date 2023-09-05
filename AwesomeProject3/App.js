aad32ad61a91e30561f9f933ff008c4d0fa88692
Your Name <you@example.com>
2023-09-06 00:32:44 +0700

    gg

--- AwesomeProject3/App.js
+++ AwesomeProject3/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on555 your dsadasdapp!</Text>
+      <Text>Open up App.js to start working on555k your dsadasdapp!</Text>
       <StatusBar style="auto" />
     </View>
   );
